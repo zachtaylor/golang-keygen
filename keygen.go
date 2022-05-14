71f371082e03dc3d88a149a96bf5fcd2dc0735d9
Zach Taylor <zach@ztaylor.me>
2022-05-14 11:45:32 -0500

    refactor MVP

--- keygen.go
+++ keygen.go
@@ -1,21 +1,37 @@
 package keygen
 
+import (
+	"math/rand"
+	"time"
+)
+
+var newRand = rand.NewSource(time.Now().UnixNano()) // allows New to mirror NewCrypto
+
+// NewCryptoFunc creates a Func that wraps NewCrypto(len)
+func NewCryptoFunc(len int) Func {
+	return func() string {
+		return NewCrypto(len)
+	}
+}
+
 // Func is a func() string
 type Func = func() string
 
-// Default uses New(32)
-func Default() string { return New(32) }
-
-// NewFunc returns a Func that calls New with len
+// NewFunc returns a Func that wraps New(len)
 func NewFunc(len int) Func {
 	return func() string {
 		return New(len)
 	}
 }
 
-// NewFunc returns a Func that calls NewCrypto with len
-func NewCryptoFunc(len int) Func {
+const defaultNewLen = 21 // evenly distribute 2*rand.int63() [126/6 = 21]
+
+// Default wraps New
+func Default() string { return New(defaultNewLen) }
+
+// DefaultFunc creates a Func that wraps Default
+func DefaultFunc() func() string {
 	return func() string {
-		return NewCrypto(len)
+		return Default()
 	}
 }
--- new.go
+++ new.go
@@ -1,14 +1,11 @@
 package keygen
 
-import (
-	"math/rand"
-	"time"
-	"unsafe"
-
-	"taylz.io/keygen/charset"
-)
+import "unsafe"
 
 // https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+
+// NotBase64 is a base64-like encoding table, except '+' and '/' are '-' and '_'
+const NotBase64 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
 
 const (
 	letterIdxBits = 6                    // 6 bits to represent a letter index
@@ -16,19 +13,16 @@ const (
 	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
-// seed is here because keygen has crypto/rand
-var src = rand.NewSource(time.Now().UnixNano())
-
 // New returns a random string of the given length
 func New(n int) string {
 	b := make([]byte, n)
 	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, newRand.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
+			cache, remain = newRand.Int63(), letterIdxMax
 		}
-		if idx := int(cache & letterIdxMask); idx < len(charset.YAB64) {
-			b[i] = charset.YAB64[idx]
+		if idx := int(cache & letterIdxMask); idx < len(NotBase64) {
+			b[i] = NotBase64[idx]
 			i--
 		}
 		cache >>= letterIdxBits
